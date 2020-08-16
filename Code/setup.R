04b221015950dada1f365bf8732e9ca90805230e
Murray Cadzow <murray.cadzow@otago.ac.nz>
2020-08-17 09:56:57 +1200

    section on making better functions Set up chunk with initial instructions

--- Code/setup.R
+++ Code/setup.R
@@ -15,20 +15,23 @@ if (!require(palmerpenguins)) {
   install.packages("palmerpengiuns")
 }
 
+
 if (!file.exists(here::here("Data/"))) {
-  dir.create("Data/")
+  dir.create(here::here("Data/"))
 }
 
-# Create the data sets
+# Create the data sets if they don't exist
+if (!(file.exists(here::here("Data/dream.csv")) &
+      file.exists(here::here("Data/biscoe.csv")) &
+      file.exists(here::here("Data/torgersen.csv")) )) {
+  islands <- unique(palmerpenguins::penguins_raw$Island)
 
-islands <- unique(palmerpenguins::penguins_raw$Island)
+  create_island_file <- function(island_name){
+    palmerpenguins::penguins_raw %>%
+      dplyr::filter(Island == island_name) %>%
+      readr::write_csv(path = here::here("Data/",stringr::str_c(tolower(island_name),".csv")))
+  }
 
-create_island_file <- function(island_name){
-  palmerpenguins::penguins_raw %>%
-    dplyr::filter(Island == island_name) %>%
-    readr::write_csv(path = here::here("Data/",stringr::str_c(tolower(island_name),".csv")))
+  purrr::walk(islands, create_island_file)
+
 }
-
-purrr::walk(islands, create_island_file)
-
-
